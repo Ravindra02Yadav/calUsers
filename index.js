20b9a1e9441846e7b831ac3dbd002ff14de46415
unknown <ravindra111223@gmail.com>
2023-01-21 12:26:09 +0530

    deploying commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const {connection} = require('./config/db');
+const {UserRouter} = require('./routes/user.route');
+
+
+const app = express();
+app.use(express.json());
+
+app.use(cors({
+    origin:'*'
+}));
+
+app.get("/",(req,res)=>{
+    res.send("Welcome To Home Routes");
+})
+
+app.use('/users', UserRouter);
+
+
+app.listen(process.env.port, async () => {
+    try{
+        await connection;
+        console.log("connected to DB successfully")
+    }
+    catch(err){
+        console.log("error connecting to DB")
+        console.log(err)
+    }
+    console.log("listening on port 8020")
+})
